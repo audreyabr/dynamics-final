36ee8c7a68fd8665fd827875a9bdd15e4adb3cee
audreyabr <78472378+audreyabr@users.noreply.github.com>
2022-05-05 15:08:46 -0400

    Update dynamics.m

--- dynamics.m
+++ dynamics.m
@@ -30,25 +30,25 @@ T_span = [0: 0.005: 20];  % time range for simulation with specified time step
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % Convert Cylindrical to Cartesian Frame
-alpha = zout(:,1);
-beta = zout(:,3);
-%delta = zout(:,5);
-
-B = [L1*sin(alpha) + L1*cos(alpha)];
-Cdiff = [L2*sin(beta) + L2*cos(beta)];
-C = B + Cdiff;
-
-% Plot Position
-figure (1)
-plot(0,0,'r')
-plot(L4,0,'r')
-plot(B(:,1), B(:,2))
-hold
-plot(C(:,1), C(:,2))
-title('Position In X-Y Space')
-xlabel('x-disp (m)')
-ylabel('y-disp (m)')
+% % % Convert Cylindrical to Cartesian Frame
+% alpha = zout(:,1);
+% beta = zout(:,3);
+% delta = zout(:,5);
+% 
+% B = [L1*sin(alpha) + L1*cos(alpha)];
+% Cdiff = [L2*sin(beta) + L2*cos(beta)];
+% C = B + Cdiff;
+% 
+% % Plot Position
+% figure (1)
+% plot(0,0,'r')
+% plot(L4,0,'r')
+% plot(B(:,1), B(:,2))
+% hold
+% plot(C(:,1), C(:,2))
+% title('Position In X-Y Space')
+% xlabel('x-disp (m)')
+% ylabel('y-disp (m)')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % Plot Position, Velocity, Tension
@@ -73,18 +73,18 @@ ylabel('y-disp (m)')
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Calculate Angular Acceleration
-accel1 = gradient(zout(:,3), 0.005)
-accel2 = gradient(zout(:,4), 0.005)
-
-% Plot Angular Acceleration
-figure (3)
-plot(t,accel1)
-hold
-plot(t, accel2)
-title('Angular Acceleration Over Time')
-xlabel('Time (s)')
-ylabel('Acceleration (rad/s^2)')
+% % Calculate Angular Acceleration
+% accel1 = gradient(zout(:,3), 0.005)
+% accel2 = gradient(zout(:,4), 0.005)
+% 
+% % Plot Angular Acceleration
+% figure (3)
+% plot(t,accel1)
+% hold
+% plot(t, accel2)
+% title('Angular Acceleration Over Time')
+% xlabel('Time (s)')
+% ylabel('Acceleration (rad/s^2)')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -92,17 +92,21 @@ ylabel('Acceleration (rad/s^2)')
     function dzdt = cylindrical(T,Z)
 
 %  z1 = alpha z2 = alphadot z3 = beta z4 = betadot z5 = delta z6 = deltadot
+    dz1dt = Z(1);
+    dz2dt = Z(2);
 
-    matrix = [-m1*L1*Z(2) 0 0 0 1 -cos(t2t1);
-              0 -m1*L1 0 0 0 sin(t2t1); 
-              -m2*L1*Z(2) 0 -m2*L2*Z(4)*cos(t2t1) -m2*L2*sin(t2t1) 0 cos(t2t1); 
-              0 -m2*L1 m2*L2*Z(4)*sin(t2t1) -m2*L2*cos(t2t1) 0 -sin(t2t1); 
-              1 0 0 0 0 0;
-              0 0 1 0 0 0];
-    vector = [m1*g*cos(Z(1)); m1*g*sin(Z(1)); m2*g*cos(Z(1)); m2*g*sin(Z(1)); Z(2); Z(4)];
-%
-dzdt = matrix\vector;
-%
+    dz3dt = (-L1*Z(2)*sin(Z(1)+Z(5))) / (L2*sin(Z(3)+Z(5)));
+    dz4dt = (L1*Z(2)*sin(Z(3)-Z(1))) / (L3*sin(Z(3)+Z(5)));
+
+    g1 = L1*Z(2)^2*cos(Z(1))+L2*Z(4)^2*cos(Z(3))+L3*Z(6)^2*cos(Z(5));
+    g2 = -L1*Z(2)^2*sin(Z(1))+L2*Z(4)^2*sin(Z(3))-L3*Z(6)^2*sin(Z(5));
+
+    dz5dt = (-g1*cos(Z(5))+g2*sin(Z(5))) / (L2*sin(Z(3)+Z(5)));
+    dz6dt = (-g1*cos(Z(3))-g2*sin(Z(3))) / (L3*sin(Z(3)+Z(5)));
+
+    dzdt = [dz1dt;dz2dt;dz3dt;dz4dt;dz5dt;dz6dt];
+
+
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -110,7 +114,7 @@ end
 function [eventvalue,stopthecalc,eventdirection] = event_stop(T,Z)
      
  % stop when the cart traverses the full horizontal distance
-        eventvalue      =  Z(1)-L    %  ‘Events’ are detected when eventvalue=0
+        eventvalue      =  Z(1)-2*pi+z1_0;    %  -Events2 are detected when eventvalue=0
         stopthecalc     =  1;       %  Stop if event occurs
         eventdirection  = 0;       %  Detect only events with dydt<0 (falling)
 end

73161e666b8f083ef771706d17a033ec445528b5
audreyabr <78472378+audreyabr@users.noreply.github.com>
2022-05-08 11:10:59 -0400

    Update dynamics_wing.m

--- dynamics_wing.m
+++ dynamics_wing.m
@@ -25,16 +25,48 @@ alpha = alpha_0+alphadot*t;
 beta = beta_0+betadot*t;
 delta = delta_0+deltadot*t;
 
-A = [0;0];
+A = [0;0]*t;
 
 B = [L1*cos(alpha); L1*sin(alpha)] + A;
 
 C = B+[beta*L2];
 
-D = [A+L4;0];
+D = [L4;0]+A;
 
-plot(C')
+wing_tot = A+B+C+D;
 
 
+
+theta_0 = 0;
+phi_0 = 45*pi/180;
+sigma_0 = 135*pi/180;
+L5 = 12;
+L6 = 4;
+L7 = 7;
+L8 = 10;
+
+epsilon = 1.0E-6;
+
+f = [L7*cos(theta_0) -L8*cos(sigma_0) +L6*cos(sigma_0) - L5; L7*sin(theta_0) -L8*sin(sigma_0) +L6*sin(sigma_0)];
+
+while norm(f)>epsilon
+    J = [-L7*sin(phi_0) L8*sin(sigma_0); L7*cos(phi_0) -L8*cos(sigma_0)];
+    dth = inv(J)*(1.0*f);
+    L7 = L7+dth(1);
+    L8 = L8+dth(2);
+    f = [L7*cos(theta_0) -L8*cos(sigma_0) +L6*cos(sigma_0) - L5; L7*sin(theta_0) -L8*sin(sigma_0) +L6*sin(sigma_0)];
+end
+
+
+for point = 1:length(wing_tot)
+        
+        % create pendulum arms vectors
+        wing_x = [0 A(point,1);
+                           A(point,1) wing_tot(point,1)];
+        wing_y = [0 A(point,2);
+                           A(point,2) wing_tot(point,2)];
+
+        plot(wing_x, wing_y,"-b","MarkerSize",5), hold on
+        
 end
 
